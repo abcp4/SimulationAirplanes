1be30c0c615159c3efd53c74bb15065e1d2b00e3
Janderocha <janderochalves@gmail.com>
2016-02-27 14:33:42 -0300

    Update Simulation.py

--- Simulation.py
+++ Simulation.py
@@ -1,1 +1,143 @@
+"""
+Carwash (3) example with Statistics computation.
+
+Modified by Mario Meireles
+
+Covers:
+
+- Waiting for other processes
+- Resources: Resource
+
+Scenario:
+  A carwash has a limited number of washing machines and defines
+  a washing processes that takes some (random) time.
+
+  Car processes arrive at the carwash at a random time. If one washing
+  machine is available, they start the washing process and wait for it
+  to finish. If not, they wait until they can use one.
+
+"""
+import random
+
+import simpy
+
+
+RANDOM_SEED = 42
+NUM_MACHINES = 3  # Numero de pistas no aeroporto
+WASHTIME = 5      # tempo que leva para pousar / decolar
+T_INTER = 7       # Chegada de um novo aviao a cada T_INTER minutos
+SIM_TIME = 50     # Tempo da simulacao
+
+class Stats:
+	""" Mantem as principais estatisticas da simulacao em curso
+	
+	"""
+	def __init__(self):
+		self.num_arrivals = 0
+		self.num_complet = 0
+		
+	def new_arrival(self):
+		self.num_arrivals+=1
+			
+	def new_completion(self):
+		self.num_complet+=1
+		
+	def report(self):
+		print ('\n*** SimPy Simulation Report ***\n')
+		print ('Total Simulation Time: %.4f' % SIM_TIME)
+		print ('Total Arrivals: %d' % self.num_arrivals)
+		print ('Total Departures: %d' % self.num_complet)
+			
+		
+class Carwash(object):
+    """A carwash has a limited number of machines (``NUM_MACHINES``) to
+    clean cars in parallel.
+
+    Cars have to request one of the machines. When they got one, they
+    can start the washing processes and wait for it to finish (which
+    takes ``washtime`` minutes).
+
+    """
+    def __init__(self, env, num_machines, washtime):
+        self.env = env
+        self.machine = simpy.Resource(env, num_machines)
+        self.washtime = washtime
+
+    def wash(self, car):
+        """The washing processes. It takes a ``car`` processes and tries
+        to clean it."""
+        yield self.env.timeout(WASHTIME)
+#         yield self.env.timeout(random.expovariate(1.0 / WASHTIME))
+
+    def partida(self,car):
+		yield self.env.timeout(WASHTIME)
+	
+	
+
+
+	
+			  
+def car(env, name, cw):
+    """The car process (each car has a ``name``) arrives at the carwash
+    (``cw``) and requests a cleaning machine.
+
+    It then starts the washing process, waits for it to finish and
+    leaves to never come back ...
+
+    """
+    print('%s Chega ao aeroporto em %.2f.' % (name, env.now))
+    stat.new_arrival()
+    arrive = env.now
+    with cw.machine.request() as request:
+        yield request
+
+        print('%s entra na pista de pouso em %.2f. Tempo de espera: %.2f' % (name, env.now, env.now - arrive))
+        start = env.now
+        yield env.process(cw.wash(name))
+
+	
+        yield env.process(cw.partida(name))
+
+        print('%s parte do aeroporto em  %.2f. Tempo do servico: %.2f' % (name, env.now, env.now - start))
+
+	
+		
+
+        stat.new_completion()
+
+
+def setup(env, num_machines, washtime, t_inter): 
+    """Create a carwash, a number of initial cars and keep creating cars
+    approx. every ``t_inter`` minutes."""
+    # Create the carwash
+    carwash = Carwash(env, num_machines, washtime)
+
+    # Create 4 initial cars
+    for i in range(4):
+        env.process(car(env, 'Car %d' % i, carwash))
+
+    # Create more cars while the simulation is running
+    while True:
+        yield env.timeout(random.randint(t_inter-2, t_inter+2))
+#         yield env.timeout(random.expovariate(1.0 / t_inter))
+        i += 1
+        env.process(car(env, 'Car %d' % i, carwash))
+
+
+# Setup and start the simulation
+print('===== Carwash =====')
+random.seed(RANDOM_SEED)  # This helps reproducing the results
+
+# Create statistics object
+stat = Stats()
+
+# Create an environment and start the setup process
+env = simpy.Environment()
+env.process(setup(env, NUM_MACHINES, WASHTIME, T_INTER))
+
+# Execute!
+env.run(until=SIM_TIME)
+
+# Report stats
+stat.report()
 
