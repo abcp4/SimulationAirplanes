7da32ea6a519309d3dcefde3fe5b6d4cd5a061e6
abcp4 <navi1921@gmail.com>
2016-04-03 10:42:42 -0300

    Added files via upload

---
+++ pseudoCode.py
@@ -0,0 +1,26 @@
+demanda = [3.3,0.019,0.001,0.074,1.31,3.4]
+demandaType = [Ld,Li,Ld,Li,Ld,Li]
+k = demanda.size
+kr = 0
+residenceTime = []
+NiMinus1R = 0
+resTime = 0
+Lk(fila com comprimentos)
+for i=1 to k:
+	if(demanda[i]>0):
+		kr++
+nr = 15(num de clientes)
+for i=1 to k:
+	if(demanda[i]>0):
+		Lk.put(nr/kr)
+	else:
+		Lk.put(0)
+tolerance = 0.0005, maxIter = 100
+while(error>tolerance and numIter>maxIter): #até convergir
+	for i=1 to k
+		NiMinus1R = Lk[i]*(nr-1)/nr
+		if(demandaType[i]=="Li"):
+			residenceTime.append(demanda[i]*(1+NiMinus1R))
+		else:
+			residenceTime.append(demanda[i])
+		resTime += 	residenceTime[i]
